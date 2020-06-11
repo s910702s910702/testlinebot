c7b2b9db6cd32c547280e4d977ef059236f119b9
David Lin <aresr417@gmail.com>
2020-06-11 20:16:25 +0800

    add ppl

--- mysite/line/models.py
+++ mysite/line/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+Class Ppl(models.Model):
+    name = models.CharField(max_length=50)
+    sid = models.CharField(max_length=50, unique = True)
+    grade = models.CharField(max_length=50)
+    pnum = models.CharField(max_length=50)
+    rnum = models.CharField(max_length=50)
+    lineuid = models.CharField(max_length=50)
