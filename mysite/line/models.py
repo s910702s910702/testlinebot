d2a0061119e4fa9793965650ab164cde146c12c8
s910702s910702 <s910702s910702@gmail.com>
2020-06-12 02:40:50 +0800

    更新註冊能力

--- mysite/line/models.py
+++ mysite/line/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Ppl(models.Model):
+    name = models.CharField(max_length=50)
+    sid = models.CharField(max_length=50, unique = True)
+    grade = models.CharField(max_length=50)
+    pnum = models.CharField(max_length=50)
+    rnum = models.CharField(max_length=50)
+    lineuid = models.CharField(max_length=50, null = True)
+    secret = models.CharField(max_length=50, null = True)       #not true
+    def __str__(self):
+        return self.name + "_" + self.sid
